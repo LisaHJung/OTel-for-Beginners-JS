e834ac8e912dfadb0a212a5450c4314c2a309e7e
LisaHJung <lisahjung@gmail.com>
2026-01-15 10:32:43 -0800

    Setup basic metrics instrumentation with OTel collector and Prometheus

--- instrumentation.js
+++ instrumentation.js
@@ -5,12 +5,19 @@ const {
 } = require('@opentelemetry/auto-instrumentations-node');
 
 const {
-  OTLPTraceExporter,
-} = require('@opentelemetry/exporter-trace-otlp-grpc');
+  OTLPMetricExporter,
+} = require('@opentelemetry/exporter-metrics-otlp-grpc');
+
+const {
+  PeriodicExportingMetricReader,
+} = require('@opentelemetry/sdk-metrics');
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new OTLPTraceExporter({
-     url: 'http://localhost:4317', 
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: 'http://localhost:4317',
+    }),
+    exportIntervalMillis: 10000,
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
